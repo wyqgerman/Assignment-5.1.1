dce1aa0328e45eec10f6604a69bf120424adf6cd
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-12-03 09:59:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment_5._1._1
+{
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            if (x < 0 || (x % 10 == 0 && x != 0))
+            {
+                return false;
+            }
+            int reversedHalf = 0;
+            while (x > reversedHalf)
+            {
+                reversedHalf = reversedHalf * 10 + x % 10;
+                x /= 10;
+            }
+            return x == reversedHalf || x == reversedHalf / 10;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.IsPalindrome(121));
+            Console.WriteLine(solution.IsPalindrome(-121));
+            Console.WriteLine(solution.IsPalindrome(10));
+        }
+    }
+}
